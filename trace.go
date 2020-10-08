53ead2e97445229c5e18d581ac3ad821d7384541
chenmeng <2786859763@qq.com>
2020-10-08 11:12:27 +0800

    init commit

---
+++ trace.go
@@ -0,0 +1,22 @@
+package debug
+
+import (
+	"log"
+	"os"
+	"runtime"
+	"time"
+)
+
+func Trace(msg string) func() {
+	start := time.Now()
+	log.Printf("enter %s", msg)
+	return func() {
+		log.Printf("exit %s (%s)", msg, time.Since(start))
+	}
+}
+
+func PrintStack() {
+	var buf [4096]byte
+	n := runtime.Stack(buf[:], false)
+	_, _ = os.Stdout.Write(buf[:n])
+}
---
+++ trace_test.go
@@ -0,0 +1,17 @@
+package debug
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestTrace(t *testing.T) {
+	defer Trace("operation")()
+	time.Sleep(2 * time.Second)
+}
+
+func TestPrintStack(t *testing.T) {
+	defer PrintStack()
+	fmt.Print(1)
+}
