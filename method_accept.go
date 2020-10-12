82d358b60b14921013ba8a37501da33f2ecf4a27
陈濛 <chenmeng.yzy@bytedance.com>
2020-10-12 22:03:32 +0800

    两种接收者

---
+++ method_accept.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+// 展示了 值接收者/指针接收者的区别
+func main() {
+	p1 := Person{}
+	fmt.Printf("address is %p\n", &p1)
+	p1.Get()
+	p1.Set()
+
+	fmt.Println()
+
+	p2 := &Person{}
+	fmt.Printf("address is %p\n", p2)
+	p2.Get()
+	p2.Set()
+
+	fmt.Println()
+
+	// 只要结构体有指针接收者方法，接口只能赋值指针
+	var age1 Age = p2
+	fmt.Printf("address is %p\n", age1)
+	age1.Get()
+	age1.Set()
+
+	fmt.Println()
+}
+
+type Person struct {
+	age int
+}
+
+func (p Person) Get() {
+	fmt.Printf("address is %p\n", &p)
+}
+
+func (p *Person) Set() {
+	fmt.Printf("address is %p\n", p)
+}
+
+type Age interface {
+	Get()
+	Set()
+}
