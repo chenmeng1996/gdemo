ba1cee68859e3dbf26fa4ca86bfc69de2e690996
陈濛 <chenmeng.yzy@bytedance.com>
2021-01-06 11:42:33 +0800

    add

---
+++ map_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type P struct {
+	a int
+}
+
+// map get返回的是值拷贝
+func TestMap1(t *testing.T) {
+	m := make(map[string]P)
+	m["a"] = P{1}
+
+	x1 := m["a"]
+	x2 := m["a"]
+	fmt.Println(x1 == x2, &x1 == &x2)
+}
+
+func TestMap2(t *testing.T) {
+	m := make(map[string]*P)
+	m["a"] = &P{1}
+
+	x1 := m["a"]
+	x2 := m["a"]
+	fmt.Println(x1 == x2, *x1 == *x2)
+}
